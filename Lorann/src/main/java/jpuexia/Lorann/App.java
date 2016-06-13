11cc58d792b4f7b4de8ba671357d101c6513c0e4
moi <moi@10.176.128.204>
2016-06-13 16:59:46 +0200

    Test commit imen version

---
+++ Lorann/src/main/java/jpuexia/Lorann/App.java
@@ -0,0 +1,13 @@
+package jpuexia.Lorann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
