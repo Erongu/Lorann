faae9015fac1462e36512deed8dcceba88a946bb
moi <moi@moi-PC>
2016-06-20 15:32:56 +0200

    fireball 1

---
+++ controller/src/main/java/controller/Demons.java
@@ -0,0 +1,40 @@
+package controller;
+
+public class Demons {
+	private boolean herotiles ;
+	private boolean Target ;
+	private int Scattertiles ; 
+	private int direction ; 
+	public static void  MoveDemons () {
+	}
+public static void ChangeDirection(){
+	
+}
+public static void Setchase(){
+	
+}
+public boolean isHerotiles() {
+	return herotiles;
+}
+public void setHerotiles(boolean herotiles) {
+	this.herotiles = herotiles;
+}
+public boolean isTarget() {
+	return Target;
+}
+public void setTarget(boolean target) {
+	Target = target;
+}
+public int getScattertiles() {
+	return Scattertiles;
+}
+public void setScattertiles(int scattertiles) {
+	Scattertiles = scattertiles;
+}
+public int getDirection() {
+	return direction;
+}
+public void setDirection(int direction) {
+	this.direction = direction;
+}
+}
